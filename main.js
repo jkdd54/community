b6c512edbde1e17c22dfcfe0098f5190de957356
jkdd54 <127365141+jkdd54@users.noreply.github.com>
2024-07-31 13:33:35 +0900

    Load Firebase SDK directly in HTML for index page

--- main.js
+++ main.js
@@ -6,21 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     const postId = urlParams.get('id');
     if (postId) {
-      const docRef = db.collection("posts").doc(postId);
-      docRef.get()
-        .then((doc) => {
-          if (doc.exists) {
-            const post = doc.data();
-            console.log('Post detail:', post);
-            postTitle.textContent = post.title;
-            postContent.textContent = post.content;
-          } else {
-            console.log("No such document!");
-          }
-        })
-        .catch((error) => {
-          console.error("Error getting document: ", error);
-        });
+      // Firebase 초기화
+      const firebaseConfig = {
+        apiKey: "AIzaSyDGKmefqcWtIPXYqZBzpBETw_IdEfp1bJQ",
+        authDomain: "worldcommunity-ed8b9.firebaseapp.com",
+        projectId: "worldcommunity-ed8b9",
+        storageBucket: "worldcommunity-ed8b9.appspot.com",
+        messagingSenderId: "261985250862",
+        appId: "1:261985250862:web:2b06b9fd30be2eb0d0b9bf",
+        measurementId: "G-5WH98LB695"
+      };
+
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+
+      const docRef = doc(db, "posts", postId);
+      getDoc(docRef).then((docSnap) => {
+        if (docSnap.exists()) {
+          const post = docSnap.data();
+          console.log('Post detail:', post);
+          postTitle.textContent = post.title;
+          postContent.textContent = post.content;
+        } else {
+          console.log("No such document!");
+        }
+      }).catch((error) => {
+        console.error("Error getting document:", error);
+      });
     }
   }
 });
